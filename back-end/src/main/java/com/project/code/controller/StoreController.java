34237ef1b8793b33e3ce7a9d1d27dc262c0b8333
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 21:50:21 +0700

    Complete POST endpoint

--- back-end/src/main/java/com/project/code/controller/StoreController.java
+++ back-end/src/main/java/com/project/code/controller/StoreController.java
@@ -11,10 +11,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Tag(name="Stores Rest API Endpoints",description="Operations related to stores")
 @RestController
@@ -42,5 +41,13 @@ public class StoreController {
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
-
+    @Operation(summary="Add new store",description="Create new store in the database")
+    @PostMapping
+    public ResponseEntity<ApiResponse<Store>> addStore(@RequestBody Store store) {
+        Store savedStore = storeService.saveStore(store);
+        ApiResponse<Store> response = new ApiResponse<>(
+                "Store",
+                savedStore);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
