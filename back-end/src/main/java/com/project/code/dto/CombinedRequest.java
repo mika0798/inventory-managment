4bf210e86c6435648f30517250af1270cbaf2bd4
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 14:18:15 +0700

    Add DTO

--- back-end/src/main/java/com/project/code/dto/CombinedRequest.java
+++ back-end/src/main/java/com/project/code/dto/CombinedRequest.java
@@ -2,28 +2,10 @@ package com.project.code.dto;
 
 import com.project.code.entity.Inventory;
 import com.project.code.entity.Product;
+import lombok.Data;
 
+@Data
 public class CombinedRequest {
-    
     private Product product;
     private Inventory inventory;
-
-    public Product getProduct() {
-        return product;
-    }
-
-    // Setter for product
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    // Getter for store
-    public Inventory getInventory() {
-        return inventory;
-    }
-
-    // Setter for store
-    public void setInventory(Inventory inventory) {
-        this.inventory = inventory;
-    }
 }
--- back-end/src/main/java/com/project/code/dto/PurchaseProductDTO.java
+++ back-end/src/main/java/com/project/code/dto/PurchaseProductDTO.java
@@ -1,51 +1,12 @@
 package com.project.code.dto;
 
+import lombok.Data;
 
+@Data
 public class PurchaseProductDTO {
     private Long id;
     private String name;
     private Double price;
     private Integer quantity;
     private Double total;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
 }
