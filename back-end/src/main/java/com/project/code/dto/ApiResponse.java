2db18ad98a1d3a0c179e23a8db94d4a717a87d9e
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 18:49:53 +0700

    Add api response

---
+++ back-end/src/main/java/com/project/code/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.project.code.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
