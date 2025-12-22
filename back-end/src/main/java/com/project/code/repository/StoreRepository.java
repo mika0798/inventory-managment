fdfc2876717be58656a09c0381647e58673b427a
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 19:26:01 +0700

    Add findById method which returns Optional<T>

--- back-end/src/main/java/com/project/code/repository/StoreRepository.java
+++ back-end/src/main/java/com/project/code/repository/StoreRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
     List<Store> findByNameContainingIgnoreCase(String name);
+    Optional<Store> findById(Long id);
 }
