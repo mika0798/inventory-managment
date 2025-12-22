a13f4f766c0f2a625b18c2a46d77166f6a0c8619
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 19:26:40 +0700

    -Create Store Controller -Complete GET endpoint validating a store by its Id

--- back-end/src/main/java/com/project/code/controller/StoreController.java
+++ back-end/src/main/java/com/project/code/controller/StoreController.java
@@ -1,35 +1,46 @@
 package com.project.code.controller;
 
+import com.project.code.dto.ApiResponse;
+import com.project.code.entity.Store;
+import com.project.code.repository.StoreRepository;
+import com.project.code.service.OrderService;
+import com.project.code.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="Stores Rest API Endpoints",description="Operations related to stores")
+@RestController
+@RequestMapping("/store")
 public class StoreController {
-// 1. Set Up the Controller Class:
-//    - Annotate the class with `@RestController` to designate it as a REST controller for handling HTTP requests.
-//    - Map the class to the `/store` URL using `@RequestMapping("/store")`.
+    private final StoreService storeService;
+    private final OrderService orderService;
+
+    @Autowired
+    public StoreController(OrderService orderService, StoreService storeService) {
+        this.storeService = storeService;
+        this.orderService = orderService;
+    }
+
+    @Operation(summary="Validate if store exists",description="Validate store by Id")
+    @GetMapping("validate/{storeId}")
+    public ResponseEntity<ApiResponse<Boolean>> validateStore(
+            @Parameter(description="Id of store")
+            @PathVariable Long storeId) {
+
+        Boolean result = storeService.storeExists(storeId);
+        ApiResponse<Boolean> apiResponse = new ApiResponse<>("Success"
+                ,result ? "Store exists" : "Store not found"
+                ,result);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
 
 
- // 2. Autowired Dependencies:
-//    - Inject the following dependencies via `@Autowired`:
-//        - `StoreRepository` for managing store data.
-//        - `OrderService` for handling order-related functionality.
-
-
- // 3. Define the `addStore` Method:
-//    - Annotate with `@PostMapping` to create an endpoint for adding a new store.
-//    - Accept `Store` object in the request body.
-//    - Return a success message in a `Map<String, String>` with the key `message` containing store creation confirmation.
-
-
- // 4. Define the `validateStore` Method:
-//    - Annotate with `@GetMapping("validate/{storeId}")` to check if a store exists by its `storeId`.
-//    - Return a **boolean** indicating if the store exists.
-
-
- // 5. Define the `placeOrder` Method:
-//    - Annotate with `@PostMapping("/placeOrder")` to handle order placement.
-//    - Accept `PlaceOrderRequestDTO` in the request body.
-//    - Return a success message with key `message` if the order is successfully placed.
-//    - Return an error message with key `Error` if there is an issue processing the order.
-
-
-   
 }
