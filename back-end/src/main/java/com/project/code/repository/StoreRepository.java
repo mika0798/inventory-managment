ca8c758c248ca5a862986bc5d9bc87b9b9323631
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 21:49:07 +0700

    Add method to check store existence

--- back-end/src/main/java/com/project/code/repository/StoreRepository.java
+++ back-end/src/main/java/com/project/code/repository/StoreRepository.java
@@ -2,8 +2,7 @@ package com.project.code.repository;
 
 import com.project.code.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +10,5 @@ import java.util.Optional;
 public interface StoreRepository extends JpaRepository<Store,Long> {
     List<Store> findByNameContainingIgnoreCase(String name);
     Optional<Store> findById(Long id);
+    boolean existByNameIgnoreCase(String name);
 }
