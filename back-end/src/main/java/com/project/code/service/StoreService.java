c7798cfcdde83d47630418492c605f25c6017d9a
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-22 19:25:17 +0700

    Create store service

---
+++ back-end/src/main/java/com/project/code/service/StoreService.java
@@ -0,0 +1,28 @@
+package com.project.code.service;
+
+import com.project.code.entity.Store;
+import com.project.code.exception.StoreNotFoundException;
+import com.project.code.repository.ProductRepository;
+import com.project.code.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreService {
+    private final StoreRepository storeRepository;
+
+    @Autowired
+    public StoreService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    public Store getStoreById(Long id){
+        return storeRepository
+                .findById(id)
+                .orElseThrow(() -> new StoreNotFoundException("Store not found"));
+    }
+
+    public boolean storeExists(Long id){
+        return storeRepository.existsById(id);
+    }
+}
