d869782ea4664cdb3d95f42f96c9c1018ddff609
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-23 12:32:28 +0700

    -Reduced boilerplate constructor code -Add validate order method -Improve api response

--- back-end/src/main/java/com/project/code/controller/StoreController.java
+++ back-end/src/main/java/com/project/code/controller/StoreController.java
@@ -3,15 +3,17 @@ package com.project.code.controller;
 import com.project.code.dto.ApiResponse;
 import com.project.code.dto.PlaceOrderRequest;
 import com.project.code.dto.StoreCreateRequest;
+import com.project.code.entity.OrderDetails;
 import com.project.code.entity.Store;
 
 import com.project.code.service.OrderService;
 import com.project.code.service.StoreService;
+import com.project.code.service.ValidateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +21,11 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name="Stores Rest API Endpoints",description="Operations related to stores")
 @RestController
 @RequestMapping("/stores")
+@RequiredArgsConstructor
 public class StoreController {
     private final StoreService storeService;
     private final OrderService orderService;
-
-    @Autowired
-    public StoreController(OrderService orderService, StoreService storeService) {
-        this.storeService = storeService;
-        this.orderService = orderService;
-    }
+    private final ValidateService validateService;
 
     @Operation(summary="Validate if store exists",description="Validate store by Id")
     @GetMapping("validate/{storeId}")
@@ -37,7 +35,8 @@ public class StoreController {
 
         Boolean result = storeService.storeExists(storeId);
         ApiResponse<Boolean> apiResponse = new ApiResponse<>(
-                result ? "Store exists" : "Store not found"
+                result ? "Success" : "Error"
+                ,result ? "Store exists" : "Store not found"
                 ,result);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
@@ -49,19 +48,27 @@ public class StoreController {
     public ResponseEntity<ApiResponse<Store>> addStore(@Valid @RequestBody StoreCreateRequest newStore) {
         Store createStore = new Store(newStore.getName(), newStore.getAddress());
         Store savedStore = storeService.saveStore(createStore);
+        Boolean result = storeService.storeExists(savedStore.getId());
         ApiResponse<Store> response = new ApiResponse<>(
-                "Store created successfully!",
+                result ? "Success" : "Error"
+                ,result ? "Store created successfully!" : "Could not create new store",
                 savedStore);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity
+                .status(result ? HttpStatus.CREATED : HttpStatus.NO_CONTENT)
+                .body(response);
     }
 
     @Operation(summary="Add an order",description="Place new order")
     @PostMapping("/placeOrder")
-    public ResponseEntity<ApiResponse<PlaceOrderRequest>> placeOrder(@RequestBody PlaceOrderRequest orderRequest) {
-        orderService.saveOrder(orderRequest);
-        ApiResponse<PlaceOrderRequest> response = new ApiResponse<>(
-                "Order has been placed",
-                orderRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    public ResponseEntity<ApiResponse<Long>> placeOrder(@RequestBody PlaceOrderRequest orderRequest) {
+        Long orderId = orderService.saveOrder(orderRequest);
+        boolean result = validateService.ValidateOrderId(orderId);
+        ApiResponse<Long> response = new ApiResponse<>(
+                result ? "Success" : "Error"
+                ,result ? "Order has been placed" : "Could not place order",
+                result ? orderId : null);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
--- back-end/src/main/java/com/project/code/service/ValidateService.java
+++ back-end/src/main/java/com/project/code/service/ValidateService.java
@@ -5,20 +5,18 @@ import com.project.code.entity.Inventory;
 import com.project.code.entity.Product;
 import com.project.code.exception.InventoryNotFoundException;
 import com.project.code.repository.InventoryRepository;
+import com.project.code.repository.OrderDetailsRepository;
 import com.project.code.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ValidateService {
     private final InventoryRepository inventoryRepository;
     private final ProductRepository productRepository;
-
-    @Autowired
-    public ValidateService(InventoryRepository inventoryRepository, ProductRepository productRepository) {
-        this.inventoryRepository = inventoryRepository;
-        this.productRepository=productRepository;
-    }
+    private final OrderDetailsRepository orderDetailsRepository;
 
     public boolean validateInventory(Inventory inventory) {
         return inventoryRepository.findByProductIdAndStoreId(
@@ -36,6 +34,10 @@ public class ValidateService {
 
     public boolean ValidateProductId(long id) {
         return productRepository.findById(id).isPresent();
+    }
+
+    public boolean ValidateOrderId(long id) {
+        return orderDetailsRepository.findById(id).isPresent();
     }
 
     public Inventory getInventoryId(Inventory inventory) {
