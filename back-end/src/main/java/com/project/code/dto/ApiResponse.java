67e148e830a59585304124a6b06c7bdba1917f9f
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-23 12:30:53 +0700

    Refactor response

--- back-end/src/main/java/com/project/code/dto/ApiResponse.java
+++ back-end/src/main/java/com/project/code/dto/ApiResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
+    private String status;
     private String message;
     private T data;
 
