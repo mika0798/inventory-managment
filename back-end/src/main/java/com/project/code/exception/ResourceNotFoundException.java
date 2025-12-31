1c7cc49a2bc33526a0b0da923735935b99ce7f03
mkn <192448446+mika0798@users.noreply.github.com>
2025-12-31 09:52:32 +0700

    Merge *NotFoundExceptions to one ResourceNotFoundException

---
+++ back-end/src/main/java/com/project/code/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.project.code.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
